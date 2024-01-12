78861d0225b43adeefe4ea4e5c19e41ca9aa5278
AMOGHPROMODPHATARPEKAR <amoghpp129@gmail.com>
2024-01-13 00:48:27 +0530

    Playlist contoller done

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -33,6 +33,24 @@ const createPlaylist = asyncHandler(async(req,res)=>{
 
 })
 
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    
+    const deleted = await Playlist.findOneAndDelete(
+        {_id:playlistId}
+    )
+        console.log(deleted)
+    if(!deleted)
+    {
+        throw new ApiError(500,"Error while deleting")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,deleted,"Deleted playlist Successfully")
+    )
+})
+
 const addVideoToPlaylist  = asyncHandler(async(req,res)=>{
     const {playlistId, videoId} = req.params
 
@@ -87,7 +105,100 @@ const addVideoToPlaylist  = asyncHandler(async(req,res)=>{
 
 })
 
+const removeVideoFromPlaylist = asyncHandler(async(req,res)=>{
+    const {playlistId, videoId} = req.params
+
+    
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set:{
+                videos:(await Playlist.findById(playlistId)).videos.filter(obj => !obj._id.equals(videoId))
+            },
+        },
+        {
+            new:true
+        }
+    )
+    if(!playlist)
+    {
+        throw new ApiError(500,"Error in deleting Video ")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,playlist,"Deleted successfully")
+    )
+
+}) 
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    
+    const playlist = await Playlist.findById(playlistId).select("-createdAt -updatedAt");
+    if(!playlist)
+    {
+        throw new ApiError(500,"Playlist not found")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,playlist,"Playlist fetched successfully")
+    )
+
+})
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    
+    const userPlaylist = await Playlist.find({owner:req.user?._id})
+    if(!userPlaylist)
+    {
+        throw new ApiError(500,"Error fetching user playlist")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,userPlaylist,"User playlist fetched successfully")
+    )
+
+})
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+    
+    const updated = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set:{
+                name:name,
+                description:description,
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!updated)
+    {
+        throw new ApiError(400,"Error while updating")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,updated,"Updated successfully")
+    )
+
+})
+
+
 export 
     {createPlaylist,
-        addVideoToPlaylist
+        addVideoToPlaylist,
+        removeVideoFromPlaylist,
+        getPlaylistById,
+        getUserPlaylists,
+        deletePlaylist,
+        updatePlaylist
 }   
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addVideoToPlaylist, createPlaylist } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router()
 
 router.route('/create').post(verifyJWT,createPlaylist)
 router.route('/add/:playlistId/:videoId').patch(verifyJWT,addVideoToPlaylist)
+router.route('/remove/:playlistId/:videoId').patch(verifyJWT,removeVideoFromPlaylist)
+router.route('/get/:playlistId').get(verifyJWT,getPlaylistById)
+router.route('/userPlaylist').get(verifyJWT,getUserPlaylists)
+router.route('/del/:playlistId').post(verifyJWT,deletePlaylist)
+router.route('/update/:playlistId').patch(verifyJWT,updatePlaylist)
+
 
 export default router
